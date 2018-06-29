7c821a22af64946e0720ff8c5ea529e568a556b1
Xander Krause <sk8rak@gmail.com>
2018-06-28 20:54:16 -0700

    Writing to the database

--- routes/index.js
+++ routes/index.js
@@ -22,4 +22,62 @@ router.get('/comicsList', function(req, res) {
     });
 });
 
+/* GET the add a new user page. */
+router.get('/newComic', function(req, res) {
+    res.render('newComic', { title: 'Add A New Comic Book' });
+});
+
+/* POST to the Add Comic Book page */
+router.post('/newComic', function(req, res) {
+
+    // Initializing the database variable
+    var db = req.db;
+
+    // Getting form values set up by attribute names
+    var bookNum = req.body.bookNum;
+    var bookTitle = req.body.bookTitle;
+    var seriesTitle = req.body.seriesTitle;
+    var issueNum = req.body.issueNum;
+    var releaseDate = req.body.releaseDate;
+    var format = req.body.format;
+    var signed = req.body.signed;
+    var graded = req.body.graded;
+    var authors = req.body.authors;
+    var penciler = req.body.penciler;
+    var inker = req.body.inker;
+    var coverArtist = req.body.coverArtist;
+    var publisher = req.body.publisher;
+    var notes = req.body.notes;
+
+    // Define which collection to use
+    var collection = db.get('comicsCollection');
+
+    // Submit to the DB
+    collection.insert({
+        "bookNum" : bookNum,
+        "bookTitle" : bookTitle,
+        "seriesTitle" : seriesTitle,
+        "issueNum" : issueNum,
+        "releaseDate" : releaseDate,
+        "format" : format,
+        "signed" : signed,
+        "graded" : graded,
+        "authors" : authors,
+        "penciler" : penciler,
+        "inker" : inker,
+        "coverArtist" : coverArtist,
+        "publisher" : publisher,
+        "notes" : notes
+    }, function (err, doc) {
+        if (err) {
+            // Return an error if the operation failed
+            res.send("The comic book could not be successfully added to the database.");
+        }
+        else {
+            // Otherwise, go on to the listing page
+            res.redirect("comicsList");
+        }
+    });
+});
+
 module.exports = router;
